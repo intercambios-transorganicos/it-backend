c9e0bc34904cde3def1b59e99bbba9730ae05564
NachoPasser <nachopasser@gmail.com>
2024-09-01 16:22:40 -0300

    Elimino host y convierto puerto en variable de entorno

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
+
 module.exports = ({ env }) => ({
-  host: env('HOST', 'localhost'),
-  port: env.int('PORT', 8082),
+  port: env.int('PORT', process.env.PORT),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b8e79284b1040641365646ec36a209a8'),
