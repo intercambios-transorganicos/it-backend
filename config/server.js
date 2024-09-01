b67ecead2876948202f5457887ac502f48ea12d8
NachoPasser <nachopasser@gmail.com>
2024-09-01 12:23:03 -0300

    Actualizo servidor a ultima versión

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', 'localhost'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8082),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b8e79284b1040641365646ec36a209a8'),
